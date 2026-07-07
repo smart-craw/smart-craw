1de9b02c5b86fac977a367d1541bdad40d1eb313
Daniel Stahl <danstahl1138@gmail.com>
2026-07-07 18:41:34 -0500

    build docker and allow ip

--- docker/nginx.entrypoint.sh
+++ docker/nginx.entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # AGENT_SERVICE includes port
 envsubst '$AGENT_SERVICE' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$ALLOWED_IP_ADDRESS' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 exec "$@"
